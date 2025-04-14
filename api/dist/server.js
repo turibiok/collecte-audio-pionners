6b0d82984561d11ce9a8281b08dd507d4ccbc3b9
TOTON Lionel <totonlionel@gmail.com>
2025-04-14 18:36:37 +0100

    Fix: retrieved the HTTPS rediretion
    rediction code from de the .htacces and the starter file has been solved

--- api/dist/server.js
+++ api/dist/server.js
@@ -7,16 +7,20 @@ const express_1 = __importDefault(require("express"));
 const audio_routes_1 = __importDefault(require("./routes/audio.routes"));
 const cors_1 = __importDefault(require("cors"));
 const app = (0, express_1.default)();
-const PORT = 3000;
-// CORS avec toutes les origines autorisées (sécurisé uniquement en local ou dev)
+// Autorise la détection du proxy
+app.set('trust proxy', true);
+// Utilisation du port provenant de l'environnement sinon 3000 par défaut
+const PORT = process.env.PORT || 3000;
+// CORS avec toutes les origines autorisées (pour dev uniquement, à adapter en prod)
 app.use((0, cors_1.default)({
-    origin: "*", // Autorise toutes les origines
+    origin: "*",
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type'],
+    allowedHeaders: ['Content-Type']
 }));
 app.use(express_1.default.json());
 // Routes
 app.use('/api', audio_routes_1.default);
+// Démarre le serveur
 app.listen(PORT, () => {
-    console.log(`Serveur sur http://localhost:${PORT}`);
+    console.log(`Serveur en écoute sur le port ${PORT}`);
 });
